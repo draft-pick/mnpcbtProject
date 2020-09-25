4986868a994504366cc9cac703c79191d864d84c
draftpick <draft-pick@yandex.ru>
2020-09-25 15:57:43 +0300

    imac

--- patients_school/url.py
+++ patients_school/url.py
@@ -3,5 +3,6 @@ from django.urls import path
 from patients_school.views import *
 
 urlpatterns = [
-    path('', stay_home, name='stay_home'),
+    path('', index, name='patients-school'),
+    path('1/', stay_home, name='stay_home'),
 ]
--- patients_school/views.py
+++ patients_school/views.py
@@ -2,6 +2,13 @@ from django.shortcuts import render
 from .models import *
 
 
+def index(request):
+    context = {
+        'title': 'Школа пациента'
+    }
+    return render(request, 'patients-school/index.html', context=context)
+
+
 def stay_home(request):
     context = {
         'title': 'Рекомендуемый режим дня больного туберкулезом без бактериовыделения, получающего лечение на дому в условиях режима "Сиди дома"'
